331faff9047c310569383035d4f7bf0a760f641b
Jaylen <76460079+DonnyPZ@users.noreply.github.com>
2023-01-18 22:27:34 -0600

    MongoDB connection

--- src/main/java/MCCore/connectDB.java
+++ src/main/java/MCCore/connectDB.java
@@ -1,13 +1,16 @@
-package core.ConnectDB;
+package MCCore;
 
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import net.md_5.bungee.api.ChatColor;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bukkit.Bukkit;
 
-public class coreMongo {
+public class connectDB {
     //Minigames
     public static MongoCollection<Document> collKit; //KitPvP Collection
     public static MongoCollection<Document> collKlash; //Klash Collection
@@ -26,18 +29,15 @@ public class coreMongo {
     public static MongoCollection<Document> collCosOut; // Collection
     public static MongoCollection<Document> collCosPets; // Collection
     public static MongoCollection<Document> collCosToys; // Collection
-    coreMongo(){
-
-    }
     public static void connectToMongo() {
-        try(MongoClient client = MongoClients.create("mongodb+srv://admin:superjman7@minecraft.fzpkp.mongodb.net/?retryWrites=true&w=majority")){
-            MongoDatabase db = client.getDatabase("skript");
-
+        try{
+            MongoClient client = MongoClients.create("mongodb+srv://admin:superjman7@minecraft.fzpkp.mongodb.net/?retryWrites=true&w=majority");
+            MongoDatabase db = client.getDatabase("MineClassic");
             //Add collections
             collKit = db.getCollection("kitpvp");
             collKlash = db.getCollection("klash");
             collEvents = db.getCollection("events");
-            //collFactions = db.getCollection("factions");
+            //Core.collFactions = db.getCollection("factions");
             collSettings = db.getCollection("settings");
             collFriends = db.getCollection("friends");
             collReport = db.getCollection("report");
@@ -48,12 +48,20 @@ public class coreMongo {
             collCosPets = db.getCollection("cosmetics-pets");
             collCosToys = db.getCollection("cosmetics-toys");
 
-            System.out.println(ChatColor.AQUA + "Successfully connected to" + ChatColor.GREEN+ "MongoDB!");
+            Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + "Successfully connected to" + ChatColor.GREEN+ " MongoDB!");
+        }catch (MongoException e){
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED+"There was an error connecting to the MongoDB Database!");
         }
-        //String uri = "mongodb+srv://admin:superjman7@minecraft.fzpkp.mongodb.net/?retryWrites=true&w=majority";
-
-
-        //Document kit = collection.find(new Document("player", "d0bff3d3-22f7-478c-b5a3-173ad74c043b")).first();
-        //System.out.println(kit.toJson());
     }
+
+    public static void updateDoc(String key, Object value, MongoCollection<Document> collection, Document doc){
+        Bson updatedValue = new Document(key, value);
+        Bson updateOperation = new Document ("$set", updatedValue);
+        collection.updateOne(doc,updateOperation);
+        //return ChatColor.GREEN+"Successfully updated MongoDB Document "+ChatColor.YELLOW+doc+ChatColor.GREEN+" in collection "+collection;
+    }
+
+    /*public Document getDocFromColl(MongoCollection<Document> collection){
+        return collection.find(new Document("player", "d0bff3d3-22f7-478c-b5a3-173ad74c043b")).first();
+    }*/
 }
