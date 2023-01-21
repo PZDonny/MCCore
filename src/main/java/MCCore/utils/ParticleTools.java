c9b53be28a4484e34ec42c2c56e0d24e12fccd94
Jaylen <76460079+DonnyPZ@users.noreply.github.com>
2023-01-21 16:59:26 -0600

    Vector parameter removed from methods. Vectors are automatically calculation by given locations

--- src/main/java/MCCore/utils/ParticleTools.java
+++ src/main/java/MCCore/utils/ParticleTools.java
@@ -8,38 +8,38 @@ import org.bukkit.util.Vector;
 public class ParticleTools {
 
 //Draw Particle Line To/From Locations
-    public static void drawParticleLine(Location l1, Location l2, Vector v, Particle particle, int particleAmnt) {
-        for(double i = 0.5; i<l1.distance(l2); i+= 0.25) {
-            Vector vector = new Vector().copy(v).multiply(i);
-            Location loc = l1.clone().add(vector);
-            loc.getWorld().spawnParticle(particle, loc, particleAmnt);
+    public static void drawParticleLine(Location from, Location to, Particle particle, int particleAmnt, double extra) {
+        for(double i = 0.5; i<from.distance(to); i+= 0.25) {
+            Vector vector  = from.clone().toVector().subtract(to.toVector()).normalize().multiply(-i);
+            Location loc = from.clone().add(vector);
+            loc.getWorld().spawnParticle(particle, loc, particleAmnt, 0, 0, 0, extra);
         }
     }
 
 //Draw Particle Line To/From Locations WITH Offsets
-    public static void drawParticleLine(Location l1, Location l2, Vector v, Particle particle, int particleAmnt, double x, double y, double z) {
-        for(double i = 0.5; i<l1.distance(l2); i+= 0.25) {
-            Vector vector = new Vector().copy(v).multiply(i);
-            Location loc = l1.clone().add(vector);
-            loc.getWorld().spawnParticle(particle, loc, particleAmnt, x, y, z);
+    public static void drawParticleLine(Location from, Location to, Particle particle, int particleAmnt, double x, double y, double z, double extra) {
+        for(double i = 0.5; i<from.distance(to); i+= 0.25) {
+            Vector vector  = from.clone().toVector().subtract(to.toVector()).normalize().multiply(-i);
+            Location loc = from.clone().add(vector);
+            loc.getWorld().spawnParticle(particle, loc, particleAmnt, x, y, z, extra);
         }
     }
 
 
 //Draw Redstone Particle Line To/From Locations
-    public static void drawRedstoneLine(Location l1, Location l2, Vector v, Color color, int particleAmnt, float size) {
-        for(double i = 0.5; i<l1.distance(l2); i+= 0.25) {
-            Vector vector = new Vector().copy(v).multiply(i);
-            Location loc = l1.clone().add(vector);
+    public static void drawRedstoneLine(Location from, Location to, Color color, int particleAmnt, float size) {
+        for(double i = 0.5; i<from.distance(to); i+= 0.25) {
+            Vector vector  = from.clone().toVector().subtract(to.toVector()).normalize().multiply(-i);
+            Location loc = from.clone().add(vector);
             loc.getWorld().spawnParticle(Particle.REDSTONE, loc, particleAmnt, 0.1, 0.1, 0.1, new Particle.DustOptions(color, size));
         }
     }
 
 //Draw Redstone Particle Line To/From Locations WITH Offsets
-    public static void drawRedstoneLine(Location l1, Location l2, Vector v, Color color, int particleAmnt, float size, double x, double y, double z) {
-        for(double i = 0.5; i<l1.distance(l2); i+= 0.25) {
-            Vector vector = new Vector().copy(v).multiply(i);
-            Location loc = l1.clone().add(vector);
+    public static void drawRedstoneLine(Location from, Location to, Color color, int particleAmnt, float size, double x, double y, double z) {
+        for(double i = 0.5; i<from.distance(to); i+= 0.25) {
+            Vector vector  = from.clone().toVector().subtract(to.toVector()).normalize().multiply(-i);
+            Location loc = from.clone().add(vector);
             loc.getWorld().spawnParticle(Particle.REDSTONE, loc, particleAmnt, x, y, z, new Particle.DustOptions(color, size));
         }
     }
