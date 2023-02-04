9de8ea406bd6759369c4937463aaea6f82b8701b
Jaylen <76460079+DonnyPZ@users.noreply.github.com>
2023-02-04 03:53:33 -0600

    Removed methods that required a cooldown parameter

--- src/main/java/MCCore/utils/AbilityTools.java
+++ src/main/java/MCCore/utils/AbilityTools.java
@@ -12,107 +12,7 @@ import java.util.*;
 
 public class AbilityTools {
 
-    //Nearby Players at Player
-    public static List<Player> getNearbyPlayers(Player p, double d, Map<UUID, Long> cooldown, boolean sendMSG, String prefix){
-        Collection<Entity> coll = p.getNearbyEntities(d, d, d);
-        if (p.getNearbyEntities(d,d,d).stream().noneMatch(en->en instanceof Player)) {
-            if (cooldown != null) cooldown.remove(p.getUniqueId());
-            if (sendMSG){
-                p.sendMessage(prefix+ChatColor.RED+"You are not within "+d+" blocks of any players!");
-                p.playSound(p, Sound.BLOCK_NOTE_BLOCK_BIT, 1, 0.5f);
-            }
-            return null;
-        }
-        Entity[] toPlayer = coll.toArray(new Entity[coll.size()]);
-        List<Player> players = new ArrayList<>();
-        for(Entity e : toPlayer){
-            if (e instanceof Player && e != p && !(e.hasMetadata("NPC"))){
-                players.add((Player) e);
-            }
-        }
-        if (players.size() > 0){
-            return players;
-        }
-        return null;
-    }
-
-    //Nearby Players at Location
-    public static List<Player> getNearbyPlayers(Player p, double d, Map<UUID, Long> cooldown, Location l, boolean sendMSG, String prefix){
-        Collection<Entity> coll = p.getWorld().getNearbyEntities(l, d, d, d);
-        if (p.getWorld().getNearbyEntities(l, d,d,d).stream().noneMatch(en->en instanceof Player)) {
-            if (cooldown != null) cooldown.remove(p.getUniqueId());
-            if (sendMSG){
-                p.sendMessage(prefix+ChatColor.RED+"You are not within "+d+" blocks of any players!");
-                p.playSound(p, Sound.BLOCK_NOTE_BLOCK_BIT, 1, 0.5f);
-            }
-            return null;
-        }
-        Entity[] toPlayer = coll.toArray(new Entity[coll.size()]);
-        List<Player> players = new ArrayList<>();
-        for(Entity e : toPlayer){
-            if (e instanceof Player && e != p && !(e.hasMetadata("NPC"))){
-                players.add((Player) e);
-            }
-        }
-        if (players.size() > 0){
-            return players;
-        }
-        return null;
-    }
-
-    //Get Targeted Player
-    public static Player returnTargetPlayer(Player p, double distance, boolean allowThorughBlocks, Map<UUID, Long> cooldown, String prefix) {
-        Location loc = p.getEyeLocation();
-        Vector v = p.getEyeLocation().getDirection();
-        float t = 0;
-        for(int i = 0; i<distance*2; i++) {
-            t += 0.5f;
-            double x = v.getX() * t;
-            double y = v.getY() * t;
-            double z = v.getZ() * t;
-            loc.add(x,y,z);
-            //loc.getWorld().spawnParticle(Particle.FLAME, loc, 20, 0, 0, 0, 0);
-            if (!allowThorughBlocks){
-                if (loc.getBlock().getType().name().contains("DOOR")){
-                    Openable door = (Openable) loc.getBlock().getBlockData();
-                    if (!door.isOpen()){
-                        denyPassthough(p, cooldown, prefix);
-                        return null;
-                    }
-                }
-                else if (!loc.getBlock().isPassable()){ //Cant use through blocks and If block is full
-                    denyPassthough(p, cooldown, prefix);
-                    return null;
-                }
-            }
-
-
-            Collection<Entity> nearby = loc.getWorld().getNearbyEntities(loc, 0.35, 0.35, 0.35);
-            loc.subtract(x,y,z); //Resets loc instead of adding again from the location further than starting point
-            if (!nearby.isEmpty()){
-                Entity[] arr = nearby.toArray(new Entity[nearby.size()]);
-                for (Entity e : arr) {
-                    if (e instanceof Player && (e != p && !(e.hasMetadata("NPC")))) {//Not the player and not an NPC
-                        return (Player) e;
-                    }
-                }
-            }
-        }
-        p.sendMessage(prefix+ChatColor.RED+"Target a player within "+ChatColor.YELLOW+distance+ChatColor.RED+" blocks of your location!");
-        p.playSound(p, Sound.BLOCK_NOTE_BLOCK_BIT, 1, 0.5f);
-        if (cooldown != null) cooldown.remove(p.getUniqueId());
-        return null;
-        //e = p.getWorld().rayTraceEntities(p.getEyeLocation(), v, 50).getHitEntity();
-    }
-    private static void denyPassthough(Player p, Map<UUID, Long> cooldown, String prefix){
-        p.sendMessage(prefix+ChatColor.RED+"You cannot target a player through blocks with this ability!");
-        p.playSound(p, Sound.BLOCK_NOTE_BLOCK_BIT, 1, 0.5f);
-        if (cooldown != null) cooldown.remove(p.getUniqueId());
-    }
-
-//Previous methods w/o cooldown
-
-    //Nearby Players at Player
+//Nearby Players at Player
     public static List<Player> getNearbyPlayers(Player p, double d, boolean sendMSG, String prefix){
         Collection<Entity> coll = p.getNearbyEntities(d, d, d);
         if (p.getNearbyEntities(d,d,d).stream().noneMatch(en->en instanceof Player)) {
@@ -135,7 +35,7 @@ public class AbilityTools {
         return null;
     }
 
-    //Nearby Players at Location
+//Nearby Players at Location
     public static List<Player> getNearbyPlayers(Player p, double d, Location l, boolean sendMSG, String prefix){
         Collection<Entity> coll = p.getWorld().getNearbyEntities(l, d, d, d);
         if (p.getWorld().getNearbyEntities(l, d,d,d).stream().noneMatch(en->en instanceof Player)) {
@@ -158,7 +58,7 @@ public class AbilityTools {
         return null;
     }
 
-    //Get Targeted Player
+//Get Targeted Player
     public static Player returnTargetPlayer(Player p, double distance, boolean allowThorughBlocks, String prefix) {
         Location loc = p.getEyeLocation();
         Vector v = p.getEyeLocation().getDirection();
@@ -168,25 +68,24 @@ public class AbilityTools {
             double x = v.getX() * t;
             double y = v.getY() * t;
             double z = v.getZ() * t;
-            loc.add(x,y,z);
-            //loc.getWorld().spawnParticle(Particle.FLAME, loc, 20, 0, 0, 0, 0);
+            Location locClone = loc.clone().add(x,y,z);
             if (!allowThorughBlocks){
-                if (loc.getBlock().getType().name().contains("DOOR")){
-                    Openable door = (Openable) loc.getBlock().getBlockData();
+                if (locClone.getBlock().getType().name().contains("DOOR")){
+                    Openable door = (Openable) locClone.getBlock().getBlockData();
                     if (!door.isOpen()){
                         denyPassthough(p, prefix);
                         return null;
                     }
                 }
-                else if (!loc.getBlock().isPassable()){ //Cant use through blocks and If block is full
+                else if (!locClone.getBlock().isPassable()){ //Cant use through blocks and If block is full
                     denyPassthough(p, prefix);
                     return null;
                 }
             }
 
 
-            Collection<Entity> nearby = loc.getWorld().getNearbyEntities(loc, 0.35, 0.35, 0.35);
-            loc.subtract(x,y,z); //Resets loc instead of adding again from the location further than starting point
+            Collection<Entity> nearby = locClone.getWorld().getNearbyEntities(locClone, 0.35, 0.35, 0.35);
+            //loc.subtract(x,y,z); //Resets loc instead of adding again from the location further than starting point
             if (!nearby.isEmpty()){
                 Entity[] arr = nearby.toArray(new Entity[nearby.size()]);
                 for (Entity e : arr) {
@@ -199,7 +98,6 @@ public class AbilityTools {
         p.sendMessage(prefix+ChatColor.RED+"Target a player within "+ChatColor.YELLOW+distance+ChatColor.RED+" blocks of your location!");
         p.playSound(p, Sound.BLOCK_NOTE_BLOCK_BIT, 1, 0.5f);
         return null;
-        //e = p.getWorld().rayTraceEntities(p.getEyeLocation(), v, 50).getHitEntity();
     }
     private static void denyPassthough(Player p, String prefix){
         p.sendMessage(prefix+ChatColor.RED+"You cannot target a player through blocks with this ability!");
