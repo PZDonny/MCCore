a9dfa093d113423fc910b594cfd9917344eab9c5
Jaylen <76460079+DonnyPZ@users.noreply.github.com>
2023-02-05 00:59:47 -0600

    Update config wording, base values are true, variables update on reload

--- src/main/java/MCCore/Core.java
+++ src/main/java/MCCore/Core.java
@@ -8,7 +8,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -16,8 +15,8 @@ import org.bukkit.scheduler.BukkitRunnable;
 import java.util.UUID;
 
 public final class Core extends JavaPlugin {
-    FileConfiguration config = getConfig();
-    public boolean projectileRandomness = getConfig().getBoolean("projectileRandomness");
+    private FileConfiguration config = getConfig();
+    public boolean projectileRandomness = getConfig().getBoolean("fixProjectileRandomness");
     public boolean connectToMongo = getConfig().getBoolean("connectToMongo");
 
     private final String serverPrefix = ChatColor.GREEN+"Mine"+ChatColor.WHITE+"Classic";
@@ -68,7 +67,14 @@ public final class Core extends JavaPlugin {
             return true;
         }
         reloadConfig();
+        config = getConfig();
+        setConfigVariables();
         sender.sendMessage(serverPrefix+ChatColor.GREEN+" Config successfully reloaded!");
         return true;
+    }
+
+    private void setConfigVariables(){
+        projectileRandomness = getConfig().getBoolean("fixProjectileRandomness");
+        connectToMongo = getConfig().getBoolean("connectToMongo");
     }
 }
