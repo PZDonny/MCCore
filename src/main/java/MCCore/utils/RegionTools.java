a593d987c2e55baa6a0bd8cdee5a4e3bba0685e2
Jaylen <76460079+DonnyPZ@users.noreply.github.com>
2023-03-28 00:32:33 -0500

    Added method to get a player's block selection

--- src/main/java/MCCore/utils/RegionTools.java
+++ src/main/java/MCCore/utils/RegionTools.java
@@ -1,13 +1,27 @@
 package MCCore.utils;
 
+import com.sk89q.worldedit.IncompleteRegionException;
+import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.regions.RegionSelector;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
+import java.util.Objects;
+
 public class RegionTools {
+
+    private final WorldEdit worldEdit = WorldEdit.getInstance();
+    private final WorldGuard worldGuard = WorldGuard.getInstance();
 
     public static boolean isPlayerinRegion(Player p, String region){
         double x = p.getLocation().getX();
@@ -19,5 +33,33 @@ public class RegionTools {
             return true;
         }
         return false;
+    }
+
+    public Block[] getSelectedBlocks(String authorsName) {
+        LocalSession worldEditSession = worldEdit.getSessionManager().findByName(authorsName);
+        if(worldEditSession != null) {
+            if(worldEditSession.getSelectionWorld() != null) {
+                RegionSelector regionSelector = worldEditSession.getRegionSelector(worldEditSession.getSelectionWorld());
+                if(regionSelector.isDefined()) {
+                    try {
+                        Region region = regionSelector.getRegion();
+                        World world = Bukkit.getWorld(Objects.requireNonNull(region.getWorld()).getName());
+                        Block[] blocks = new Block[(int) region.getVolume()];
+
+                        int i = 0;
+                        for(BlockVector3 blockVector3 : region) {
+                            blocks[i] = new Location(world, blockVector3.getX(), blockVector3.getY(), blockVector3.getZ()).getBlock();
+                            i++;
+                        }
+
+                        return blocks;
+                    } catch (IncompleteRegionException e) {
+                        e.printStackTrace();
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+        return new Block[0];
     }
 }
