c5a9617d8d00fd4d24e48765dd5d958969591317
Jaylen <76460079+DonnyPZ@users.noreply.github.com>
2023-03-28 00:13:44 -0500

    Added Party Chat

---
+++ src/main/java/MCCore/listeners/Chat.java
@@ -0,0 +1,33 @@
+package MCCore.listeners;
+
+import MCCore.Core;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Chat implements Listener {
+
+    public static Map<UUID, Integer> playerChatChannels = new ConcurrentHashMap<>();
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void playerChat(AsyncPlayerChatEvent e){
+        Player p = e.getPlayer();
+        int chatChannel = playerChatChannels.get(p.getUniqueId());
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+        out.writeUTF(p.getUniqueId().toString());
+        out.writeUTF(e.getMessage());
+        if (chatChannel == 1) {
+            e.setCancelled(true);
+            p.sendPluginMessage(Core.getInstance(), "mccore:partychat", out.toByteArray());
+        }
+    }
+}
