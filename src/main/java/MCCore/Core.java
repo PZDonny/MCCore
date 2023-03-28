a6ba15ed6209ddff4fa6d81c55bd416d784ccf6c
Jaylen <76460079+DonnyPZ@users.noreply.github.com>
2023-03-28 00:16:44 -0500

    Updated to 0.4.0

--- src/main/java/MCCore/Core.java
+++ src/main/java/MCCore/Core.java
@@ -1,8 +1,13 @@
 package MCCore;
 
 import MCCore.commands.*;
-import MCCore.events.ProjectileRandomness;
-import net.md_5.bungee.api.ChatColor;
+import MCCore.listeners.*;
+import MCCore.minigameAPI.SlimeTools;
+import MCCore.sockets.Client;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -10,20 +15,67 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.io.IOException;
+import java.net.Socket;
+
 
 public final class Core extends JavaPlugin {
     private FileConfiguration config = getConfig();
-    public boolean projectileRandomness = getConfig().getBoolean("fixProjectileRandomness");
-    public boolean connectToMongo = getConfig().getBoolean("mongoDB.enabled");
-    public String connectionString = getConfig().getString("mongoDB.connectionString");
 
-    private final String serverPrefix = ChatColor.DARK_RED+"Sequestered";
+    private static Core instance;
+    public static boolean projectileRandomness;
+    public static boolean connectToMongo;
+    public static String connectionString;
+
+    public static String fallbackServer;
+
+    public static boolean isPlaytest;
+
+    public static int maxArenas;
+
+    public static boolean playCommandAllowed;
+
+    private static Client client;
+
+    public static int port;
+
+    static World waitingWorld;
+
+    public static String prefix = "["+ ChatColor.GREEN+"M"+ChatColor.WHITE+"C"+ChatColor.RED+" Core"+ChatColor.WHITE+"] ";
+
     @Override
     public void onEnable() {
+
+        //SlimeWorldManager
+        if (!Bukkit.getPluginManager().isPluginEnabled("SlimeWorldManager")) {
+            getLogger().severe("MCCore cannot fully utilize the MinigameAPI!");
+            getLogger().severe("*** SlimeWorldManager is not installed or not enabled. ***");
+        }
+
+        //WorldGuard
+        if (!Bukkit.getPluginManager().isPluginEnabled("WorldGuard")) {
+            getLogger().severe("MCCore cannot fully utilize the WorldTools Utility!");
+            getLogger().severe("*** WorldGuard is not installed or not enabled. ***");
+        }
+
+        instance = this;
+        new PluginMessage();
+
+    //Slime World Variables
+        SlimeTools.setSlimeVariables();
+
+    //Configuartion
         config.options().copyDefaults(true);
         saveConfig();
-        //WGAPI = getWorldGuard();
+        setConfigVariables();
+        try {
+            client = new Client(new Socket(Bukkit.getServer().getIp(), port));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "M" + ChatColor.WHITE + "C" + ChatColor.DARK_RED + "CORE " + ChatColor.GREEN + "ENABLED");
+
         new BukkitRunnable() {//Connect to Mongo Asynchronously
             public void run() {
                 if (connectToMongo) connectDB.connectToMongo(connectionString);
@@ -43,35 +95,72 @@ public final class Core extends JavaPlugin {
         getCommand("noon").setExecutor(new DayCycle());
         getCommand("night").setExecutor(new DayCycle());
         getCommand("midnight").setExecutor(new DayCycle());
-        //getCommand("ping").setExecutor(new Ping());
         getCommand("corereload").setExecutor(this);
+        getCommand("play").setExecutor(new TempPlayCommand());
 
         //Events
-        getServer().getPluginManager().registerEvents(new ProjectileRandomness(this), this);
+        getServer().getPluginManager().registerEvents(new ProjectileRandomness(), this);
+        getServer().getPluginManager().registerEvents(new PlayerChangeWorld(), this);
+        getServer().getPluginManager().registerEvents(new JoinQuit(), this);
+        getServer().getPluginManager().registerEvents(new Chat(), this);
+
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "mccore:connect");
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "mccore:partychat");
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "mccore:minigameapi");
+
+        this.getServer().getMessenger().registerIncomingPluginChannel(this, "mccore:minigameapi", PluginMessage.getInstance());
+        this.getServer().getMessenger().registerIncomingPluginChannel(this, "mccore:changechat", PluginMessage.getInstance());
+
 
     }
 
     @Override
     public void onDisable() {
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "M" + ChatColor.WHITE + "C" + ChatColor.DARK_RED + "CORE " + ChatColor.RED + "DISABLED");
+        client.disconnect();
         // Plugin shutdown logic
+    }
+
+    public static Core getInstance(){
+        return instance;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (sender instanceof Player && !(sender.hasPermission("sq.core.reload"))) {
+        if (sender instanceof Player && !(sender.hasPermission("mccore.reload"))) {
             sender.sendMessage(ChatColor.RED+"You do not have permission to do this command!");
             return true;
         }
         reloadConfig();
         config = getConfig();
         setConfigVariables();
-        sender.sendMessage(serverPrefix+ChatColor.GREEN+" Config successfully reloaded!");
+        sender.sendMessage(prefix+ChatColor.GREEN+" Config successfully reloaded!");
         return true;
     }
 
     private void setConfigVariables(){
         projectileRandomness = getConfig().getBoolean("fixProjectileRandomness");
-        connectToMongo = getConfig().getBoolean("connectToMongo");
+        connectToMongo = getConfig().getBoolean("mongoDB.enabled");
+        isPlaytest = getConfig().getBoolean("mongoDB.usePlaytestDatabase");
+        connectionString = getConfig().getString("mongoDB.connectionString");
+        playCommandAllowed = getConfig().getBoolean("minigames.allowPlayCommand");
+        maxArenas = getConfig().getInt("minigames.maxArenas");
+        port = getConfig().getInt("port");
+        fallbackServer = getConfig().getString("minigames.mainFallbackServer");
+        String world = getConfig().getString("minigames.waitingWorld");
+        if (world != null && Bukkit.getWorld(world) != null){
+            waitingWorld = Bukkit.getWorld(world);
+        }
+        else{
+            Bukkit.getConsoleSender().sendMessage(Component.text(ChatColor.RED+"There was an error using the specified minigame waiting world!"+ChatColor.GOLD+"("+world+")"));
+        }
+    }
+
+    public static Client getClient() {
+        return client;
+    }
+
+    public World getMinigameWaitingWorld(){
+        return waitingWorld;
     }
 }
