d0e9956b82e72fa9541aaa05f51118dcd402191d
Jay <76460079+DonnyPZ@users.noreply.github.com>
2023-09-19 02:33:35 -0500

    MinigameAPI and Abilities (0.5.0)

--- src/main/java/MCCore/listeners/Damage.java
+++ src/main/java/MCCore/listeners/Damage.java
@@ -1,6 +1,7 @@
 package MCCore.listeners;
 
 import MCCore.Core;
+import MCCore.utils.AbilityHandler;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,13 +15,22 @@ public class Damage implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onDamage(EntityDamageEvent e){
         Entity entity = e.getEntity();
-        if (Core.getInstance().getMinigameWaitingWorld() != null){
-            if (entity instanceof Player){
-                Player p = (Player) entity;
-                if (p.getWorld().equals(Core.getInstance().getMinigameWaitingWorld())) e.setCancelled(true);
+        if (entity instanceof Player){
+            Player p = (Player) entity;
+        //Minigame Waiting World
+            if (Core.getInstance().getMinigameWaitingWorld() != null){
+                if (p.getWorld().equals(Core.getInstance().getMinigameWaitingWorld())){
+                    e.setCancelled(true);
+                    return;
+                }
             }
-
+        //Ability Fall Damage Resistance
+            if (e.getCause().equals(EntityDamageEvent.DamageCause.FALL) && AbilityHandler.isPlayerFallDamageResistant(p)){
+                e.setCancelled(true);
+                return;
+            }
         }
+
 
 
     }
