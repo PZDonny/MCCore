34f0723e371a3781785de9255a37607fe9084b80
Jay <76460079+DonnyPZ@users.noreply.github.com>
2023-09-19 02:10:41 -0500

    Updated Socket Connections (0.5.0)

--- src/main/java/MCCore/sockets/Client.java
+++ src/main/java/MCCore/sockets/Client.java
@@ -1,9 +1,21 @@
 package MCCore.sockets;
 
+import MCCore.Core;
+import MCCore.events.SongNamesReceivedEvent;
+import MCCore.events.SongReceivedEvent;
+import com.xxmicloxx.NoteBlockAPI.model.Song;
+import com.xxmicloxx.NoteBlockAPI.utils.NBSDecoder;
+import net.kyori.adventure.text.Component;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Client {
 
@@ -12,25 +24,88 @@ public class Client {
     private ObjectInputStream input;
     private ObjectOutputStream output;
 
-    public Client(Socket socket) {
+    Timer keepAliveTimer = new Timer();
+
+    public Client(String dataProxyIP, int port) {
+        new Thread(()->{
+            try{
+                this.socket = new Socket(dataProxyIP, port);
+                this.output = new ObjectOutputStream(socket.getOutputStream());
+                this.input = new ObjectInputStream(socket.getInputStream());
+                listenForMessage();
+                //String[] out = new String[]{"spigot", String.valueOf(Bukkit.getServer().getPort())};
+                String serverType;
+                if (Core.isLobbyServer()){
+                    serverType = "spigot_lobby";
+                }
+                else{
+                    serverType = "spigot";
+                }
+                String[] out = new String[]{serverType, Core.getServerID(), String.valueOf(Bukkit.getServer().getPort())};
+                sendMessage(out);
+                sendKeepAlivePackets();
+                Bukkit.getConsoleSender().sendMessage(new TextComponent(Core.prefix+ ChatColor.GREEN+"Successfully connected to data proxy!"));
+
+                if (!ClientReconnector.hasInstance()){
+                    new ClientReconnector(Core.getDataProxyIP(), Core.getPort(), this);
+                }
+            } catch (IOException e) {
+                if (!ClientReconnector.hasInstance()){
+                    new ClientReconnector(Core.getDataProxyIP(), Core.getPort(), null);
+                }
+                Bukkit.getConsoleSender().sendMessage(Core.prefix+ ChatColor.RED+"Failed to connect to data proxy!");
+                Bukkit.getConsoleSender().sendMessage(Core.prefix+"Reconnecting...");
+                closeEverything();
+            }
+        }).start();
+    }
+
+    Client(Socket socket) {
         try{
             this.socket = socket;
             this.output = new ObjectOutputStream(socket.getOutputStream());
             this.input = new ObjectInputStream(socket.getInputStream());
             listenForMessage();
-            String[] out = new String[]{"connect_"+Bukkit.getServer().getPort()};
-            //ByteArrayDataOutput out = ByteStreams.newDataOutput();
-            //out.writeUTF("connect_"+Bukkit.getServer().getPort());
+            String[] out = new String[]{"spigot", Core.getServerID(), String.valueOf(Bukkit.getServer().getPort())};
             sendMessage(out);
-            //sendMessage(new String[]{"connect_"+Bukkit.getServer().getPort()});
+            sendKeepAlivePackets();
+            Bukkit.getConsoleSender().sendMessage(new TextComponent(Core.prefix+ ChatColor.GREEN+"Successfully connected to data proxy!"));
+            if (!ClientReconnector.hasInstance()) new ClientReconnector(Core.getDataProxyIP(), Core.getPort(), this);
         } catch (IOException e) {
+            if (!ClientReconnector.hasInstance()) new ClientReconnector(Core.getDataProxyIP(), Core.getPort(), null);
+            Bukkit.getConsoleSender().sendMessage(Core.prefix+ ChatColor.RED+"Failed to connect to data proxy!");
+            Bukkit.getConsoleSender().sendMessage(Core.prefix+"Reconnecting...");
             closeEverything();
         }
     }
 
+    //Doesn't allow socket to timeout
+    private void sendKeepAlivePackets(){
+        keepAliveTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (!socket.isConnected() || socket.isClosed()){
+                    cancel();
+                    return;
+                }
+                sendMessage(new String[]{"0"});
+            }
+        },15000, 15000);
+    }
+
+    public boolean isSocketConnected(){
+        return socket.isConnected();
+    }
+
+    public Socket getSocket(){
+        return this.socket;
+    }
+
     public void sendMessage(String[] message){
+        if (socket == null || socket.isClosed() || output == null) return;
         try{
             output.writeObject(message);
+            output.flush();
         }
         catch (IOException e){
             e.printStackTrace();
@@ -40,13 +115,45 @@ public class Client {
     private void listenForMessage(){
         new Thread(() -> {
             while (socket.isConnected()){
+                if (socket.isClosed()){
+                    disconnect();
+                    return;
+                }
                 try{
-                    String[] message = (String[]) input.readObject();
-                    if (message[0].contains("minigameapi")) MinigameAPIMessages.run(message);
-
+                    Object[] objectArray = (Object[]) input.readObject();
+                    if (objectArray instanceof String[]){
+                        String[] message = (String[]) objectArray;
+                        if (message[0].contains("mgapi")) MinigameAPIMessages.run(message);
+                        else if (message[0].equalsIgnoreCase(Messages.DISCONNECT.getID())) disconnect();
+                    }
+                    else {
+                        String tag = (String) objectArray[0];
+                        if (tag.equals(Messages.NBAPI_GETSONGNAMES.getID())){
+                            ArrayList<String> songNames = (ArrayList<String>) objectArray[1];
+                            String requestTag = (String) objectArray[2];
+                            new BukkitRunnable(){
+                                public void run(){
+                                    Bukkit.getPluginManager().callEvent(new SongNamesReceivedEvent(songNames, requestTag));
+                                }
+                            }.runTask(Core.getInstance());
+                        }
+                        else if (tag.equals(Messages.NBAPI_GETSONG.getID())){
+                            byte[] bytes =(byte[]) objectArray[1];
+                            String requestTag = (String) objectArray[2];
+                            DataInputStream byteSong = new DataInputStream(new ByteArrayInputStream(bytes));
+                            Song song = NBSDecoder.parse(byteSong);
+                            new BukkitRunnable(){
+                                public void run(){
+                                    Bukkit.getPluginManager().callEvent(new SongReceivedEvent(song, requestTag));
+                                }
+                            }.runTask(Core.getInstance());
+                        }
+                    }
                 } catch (IOException e) {
-                    closeEverything();
+                    disconnect();
+                    return;
                 } catch (ClassNotFoundException e) {
+                    Bukkit.getConsoleSender().sendMessage(Component.text(Core.prefix+ChatColor.RED+"An error when attempting to receive data!"));
                     e.printStackTrace();
                 }
             }
@@ -54,16 +161,25 @@ public class Client {
     }
 
     public void disconnect(){
+        Bukkit.getConsoleSender().sendMessage(Component.text(Core.prefix+ChatColor.YELLOW+"Closing connection with data proxy!"));
         closeEverything();
     }
 
+
     private void closeEverything(){
+        keepAliveTimer.cancel();
         try{
-            if (output != null) output.close();
+            if (socket != null){
+                socket.close();
+            }
 
-            if (input != null) input.close();
+            if (output != null){
+                output.close();
+            }
 
-            if (socket != null) socket.close();
+            if (input != null){
+                input.close();
+            }
         }
         catch (IOException e) {
             e.printStackTrace();
