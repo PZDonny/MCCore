254096c9c1f41a2cc09f1fdc3b541b2b76e4f42b
Jay <76460079+DonnyPZ@users.noreply.github.com>
2023-09-19 02:29:30 -0500

    Proper MinigameAPI implementation and socket messaging (0.5.0)

--- src/main/java/MCCore/commands/PlayCommand.java
+++ src/main/java/MCCore/commands/PlayCommand.java
@@ -1,24 +1,26 @@
-package MCCore.tempMinigame;
+package MCCore.commands;
 
-import MCCore.minigameAPI.MinigameJoin;
+import MCCore.minigameAPI.PlayMinigame;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public class TempPlayCommand implements CommandExecutor {
+public class PlayCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (!(sender instanceof Player)){
             return true;
         }
-        if (args.length == 0){
+        if (args.length < 2){
+            sender.sendMessage(ChatColor.RED+"Incorrect Usage! /play <minigame> <mode>");
             return false;
         }
         else{
             Player p = (Player) sender;
-            MinigameJoin.attempt(p, args[0], "normal");
+            PlayMinigame.join(p, args[0], args[1], true);
         }
         return true;
     }
