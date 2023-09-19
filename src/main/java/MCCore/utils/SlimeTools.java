a060b6ec150ee9c0b14324e95c6c505849f56cbf
Jay <76460079+DonnyPZ@users.noreply.github.com>
2023-09-19 02:37:08 -0500

    Added method to get world from datasource (0.5.0)

--- src/main/java/MCCore/utils/SlimeTools.java
+++ src/main/java/MCCore/utils/SlimeTools.java
@@ -2,9 +2,13 @@ package MCCore.utils;
 
 import MCCore.Core;
 import com.infernalsuite.aswm.api.SlimePlugin;
+import com.infernalsuite.aswm.api.exceptions.*;
 import com.infernalsuite.aswm.api.loaders.SlimeLoader;
 import com.infernalsuite.aswm.api.world.SlimeWorld;
+import com.infernalsuite.aswm.api.world.properties.SlimePropertyMap;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.IOException;
 
@@ -13,11 +17,14 @@ public class SlimeTools {
     private static SlimePlugin slimePlugin;
     private static SlimeLoader slimeLoader;
 
-    public static void setSlimeVariables(){
+    public static boolean setSlimeVariables(){
         if (Core.isSlimeInstalled()){
             slimePlugin = (SlimePlugin) Bukkit.getPluginManager().getPlugin("SlimeWorldManager");
             slimeLoader = slimePlugin.getLoader("mongodb");
+            if (slimeLoader == null) return false;
+            return true;
         }
+        return false;
 
     }
 
@@ -45,4 +52,51 @@ public class SlimeTools {
 
     }
 
+    public static SlimeWorld getCloneFromDataSource(String worldName, String cloneWorldName, boolean autoLoad){
+        SlimeWorld world = slimePlugin.getWorld(worldName);
+        if (world == null){
+            try{
+                //Fetches and loads from loader
+                world = slimePlugin.loadWorld(slimeLoader, worldName, true, new SlimePropertyMap());
+                if (world == null){
+                    throw new UnknownWorldException(worldName);
+                }
+            }
+            catch (WorldLockedException | IOException | UnknownWorldException | NewerFormatException |
+                   CorruptedWorldException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        try{
+            SlimeWorld cloneWorld = world.clone(cloneWorldName, null);
+
+        //Unload Template
+            //WorldTools.destroyWorld(Bukkit.getWorld(worldName));
+
+
+
+        //Auto Load Clone
+            if (autoLoad){
+                new BukkitRunnable(){
+                    @Override
+                    public void run() {
+                        slimePlugin.loadWorld(cloneWorld);
+                        World w = Bukkit.getWorld(cloneWorldName);
+                        if (w != null){
+                            w.setKeepSpawnInMemory(false);
+                            w.setAutoSave(false);
+                        }
+                    }
+                }.runTask(Core.getInstance());
+
+
+            }
+
+            return cloneWorld;
+        }
+        catch (IOException | WorldAlreadyExistsException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
