3420e480becad204ee6b042ff5d2aee9bd9c462d
Jay <76460079+DonnyPZ@users.noreply.github.com>
2023-12-27 21:09:51 -0500

    Disable spectator teleportation

---
+++ src/main/java/MCCore/listeners/Teleport.java
@@ -0,0 +1,30 @@
+package MCCore.listeners;
+
+import MCCore.Core;
+import MCCore.minigameAPI.GameState;
+import MCCore.minigameAPI.arenaManager.Arena;
+import MCCore.minigameAPI.arenaManager.ArenaManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+public class Teleport implements Listener {
+
+    @EventHandler
+    public void onTP(PlayerTeleportEvent e){
+        if (!Core.isMinigameEnabled()){
+            return;
+        }
+
+        Player p = e.getPlayer();
+        Arena arena = ArenaManager.getArenaOfPlayer(p);
+        if (arena == null || arena.getGameState() == GameState.CONNECTING || arena.isEndingOrNotUsable()){
+            return;
+        }
+
+        if (e.getCause() == PlayerTeleportEvent.TeleportCause.SPECTATE){
+            e.setCancelled(true);
+        }
+    }
+}
