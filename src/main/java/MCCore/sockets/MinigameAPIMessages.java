6380e341a951bca445c218244a471400ac4562bd
Jay <76460079+DonnyPZ@users.noreply.github.com>
2023-12-27 20:58:16 -0500

    Updated to read all objects instead of just string

--- src/main/java/MCCore/sockets/MinigameAPIMessages.java
+++ src/main/java/MCCore/sockets/MinigameAPIMessages.java
@@ -1,83 +1,96 @@
 package MCCore.sockets;
 
 import MCCore.Core;
+import MCCore.events.ArenaCreatedEvent;
 import MCCore.events.PlayerRemovedFromArenaEvent;
-import MCCore.utils.SlimeTools;
+import MCCore.minigameAPI.ConnectedParty;
 import MCCore.minigameAPI.arenaManager.Arena;
 import MCCore.minigameAPI.arenaManager.ArenaManager;
-import com.infernalsuite.aswm.api.world.SlimeWorld;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 public class MinigameAPIMessages {
 
-    public static void run(String[] message){
-        try{
-            String tag = message[0];
-            if (tag.equals(Messages.MINIGAMEAPI_JOINARENA.getID())){
-                String uuids = message[1];
-                int min = Integer.parseInt(message[2]);
-                int queueID = Integer.parseInt(message[3]);
-                String mode = message[4];
-                String minigame = message[5];
-                OfflinePlayer host = null;
-                if (!message[6].equals("null")){
-                    host = Bukkit.getOfflinePlayer(UUID.fromString(message[6]));
-                }
+    public static void run(Object[] message){
+        String tag = (String) message[0];
+        if (tag.equals(Messages.MINIGAMEAPI_CREATEARENA.getID())){
+            String uuids = (String) message[1];
+            int minPlayers = (int) message[2];
+            int queueID = (int) message[3];
+            String mode = (String) message[4];
+            String minigame = (String) message[5];
+            OfflinePlayer host = null;
+            String privateSettings = (String) message[7];
+            ArrayList<String[]> connectedParties = (ArrayList<String[]>) message[8];
+            if (!message[6].equals("")){
+                host = Bukkit.getOfflinePlayer(UUID.fromString((String) message[6]));
+            }
 
-                List<String> uuidsAsString = new ArrayList<>(Arrays.asList(uuids.replace(" ", "").replace("[", "").replace("]", "").split(",")));
-                List<UUID> playerList = new ArrayList<>();
-                for (String uuid : uuidsAsString){
-                    playerList.add(UUID.fromString(uuid));
-                    Player p = Bukkit.getPlayer(UUID.fromString(uuid));
-                    if (p != null && ArenaManager.getArenaOfPlayer(p) != null){
-                        new BukkitRunnable(){
-                            public void run(){
-                                ArenaManager.removePlayerFromArena(p, false, PlayerRemovedFromArenaEvent.RemoveCause.PLUGIN);
-                            }
-                        }.runTask(Core.getInstance());
+            List<String> uuidsAsString = new ArrayList<>(Arrays.asList(uuids.replace(" ", "").replace("[", "").replace("]", "").split(",")));
+            List<UUID> playerList = new ArrayList<>();
+            for (String uuid : uuidsAsString){
+                playerList.add(UUID.fromString(uuid));
+                Player p = Bukkit.getPlayer(UUID.fromString(uuid));
 
-                    }
-                }
-
-                //Check if players should be added to an existing arena or if a new one must be created
-                if (!ArenaManager.addPlayersToAvaliableArena(playerList, queueID)){
-                    List<String> worldList = SlimeTools.getSlimeLoader().listWorlds();
-                    SlimeWorld world = SlimeTools.getSlimePlugin().getWorld(worldList.get(new Random().nextInt(worldList.size())));
-                    Arena arena = new Arena(queueID, host);
-                    arena.setMinigame(minigame, mode);
-                    arena.setMinimumPlayers(min);
-                    ArenaManager.createArena(arena, playerList, true);
+            //Remove From Arena If Already Playing
+                if (p != null && ArenaManager.getArenaOfPlayer(p) != null){
+                    new BukkitRunnable(){
+                        public void run(){
+                            ArenaManager.removePlayerFromArena(p, false, PlayerRemovedFromArenaEvent.RemoveCause.JOINEDNEW);
+                            p.teleport(Core.getInstance().getMinigameWaitingWorld().getSpawnLocation());
+                            ArenaManager.refreshPlayer(p, GameMode.ADVENTURE);
+                        }
+                    }.runTask(Core.getInstance());
                 }
             }
 
-            else if (tag.equals(Messages.MINIGAMEAPI_ADDTOEXISTING.getID())){
-                String uuids = message[1];
-                int queueID = Integer.parseInt(message[2]);
-                List<String> uuidsAsString = new ArrayList<>(Arrays.asList(uuids.replace(" ", "").replace("[", "").replace("]", "").split(",")));
-                List<UUID> playerList = new ArrayList<>();
+            //if (!ArenaManager.addPlayersToExistingArena(playerList, queueID)){
+                Arena arena = new Arena(queueID, host, privateSettings);
+                arena.setMinigame(minigame, mode);
+                arena.setMinimumPlayers(minPlayers);
+                ArenaManager.startArena(arena, playerList);
 
-                for (String uuid : uuidsAsString){
-                    playerList.add(UUID.fromString(uuid));
-                    Player p = Bukkit.getPlayer(UUID.fromString(uuid));
-                    if (p != null && ArenaManager.getArenaOfPlayer(p) != null){
-                        new BukkitRunnable(){
-                            public void run(){
-                                ArenaManager.removePlayerFromArena(p, false, PlayerRemovedFromArenaEvent.RemoveCause.PLUGIN);
-                            }
-                        }.runTask(Core.getInstance());
-                    }
+            //Get parties of players and add them to the arena
+                for (String[] partyArray : connectedParties){
+                    arena.addConnectedParty(new ConnectedParty(partyArray));
                 }
-                ArenaManager.addPlayersToAvaliableArena(playerList, queueID);
-            }
-        } catch (IOException e){
-            e.printStackTrace();
+                new BukkitRunnable(){
+                    @Override
+                    public void run() {
+                        new ArenaCreatedEvent(arena).callEvent();
+                    }
+                }.runTask(Core.getInstance());
+            //}
+            return;
         }
+        if (message instanceof String[] stringArray){
+            if (tag.equals(Messages.MINIGAMEAPI_ADDTOEXISTING.getID())){
+                String uuids = stringArray[1];
+                int queueID = Integer.parseInt(stringArray[2]);
+                List<String> uuidsAsString = new ArrayList<>(Arrays.asList(uuids.replace(" ", "").replace("[", "").replace("]", "").split(",")));
+                List<UUID> playerList = new ArrayList<>();
 
+                for (String uuid : uuidsAsString){
+                    playerList.add(UUID.fromString(uuid));
+                    Player p = Bukkit.getPlayer(UUID.fromString(uuid));
+                    if (p != null && ArenaManager.getArenaOfPlayer(p) != null){
+                        new BukkitRunnable(){
+                            public void run(){
+                                ArenaManager.removePlayerFromArena(p, false, PlayerRemovedFromArenaEvent.RemoveCause.JOINEDNEW);
+                            }
+                        }.runTask(Core.getInstance());
+                    }
+                }
+                ArenaManager.setQueueTargetArena(playerList, queueID);
+            }
+        }
     }
 }
