b077fc72d2677d361b5788908f4eaf323e921c99
Jay <76460079+DonnyPZ@users.noreply.github.com>
2023-12-27 20:59:35 -0500

    getArenaContainer()

--- src/main/java/MCCore/minigameAPI/arenaManager/MinigamePlayerProfile.java
+++ src/main/java/MCCore/minigameAPI/arenaManager/MinigamePlayerProfile.java
@@ -4,12 +4,8 @@ import MCCore.utils.Scoreboard.PlayerScoreboard;
 import MCCore.utils.Scoreboard.ScoreboardUtils;
 import org.bukkit.OfflinePlayer;
 
-import java.util.HashMap;
-import java.util.UUID;
-
 public abstract class MinigamePlayerProfile {
 
-    private static final HashMap<UUID, MinigamePlayerProfile> allProfiles = new HashMap<>();
 
     protected final OfflinePlayer player;
 
@@ -17,24 +13,17 @@ public abstract class MinigamePlayerProfile {
     protected int deaths = 0;
 
     protected boolean isRespawning = false;
+    protected final ArenaContainer arenaContainer;
 
-    public MinigamePlayerProfile(OfflinePlayer player){
+    public MinigamePlayerProfile(OfflinePlayer player, ArenaContainer arenaContainer){
         this.player = player;
-        allProfiles.put(player.getUniqueId(), this);
-    }
-
-    public static MinigamePlayerProfile getPlayerProfile(OfflinePlayer p){
-        return allProfiles.get(p.getUniqueId());
-    }
-
-    void deleteProfile(){
-        allProfiles.remove(player.getUniqueId());
+        this.arenaContainer = arenaContainer;
+        arenaContainer.profiles.put(player.getUniqueId(), this);
     }
 
     public void addKill(){
         kills++;
         PlayerScoreboard board = ScoreboardUtils.getPlayerScoreboard(player.getUniqueId());
-        PlayerScoreboard.UpdatingValue.PROFILE_KILLS.updateValue(board, this);
         PlayerScoreboard.UpdatingValue.PROFILE_KILLS.updateValue(board, this);
     }
 
@@ -42,20 +31,17 @@ public abstract class MinigamePlayerProfile {
         kills--;
         PlayerScoreboard board = ScoreboardUtils.getPlayerScoreboard(player.getUniqueId());
         PlayerScoreboard.UpdatingValue.PROFILE_KILLS.updateValue(board, this);
-        PlayerScoreboard.UpdatingValue.PROFILE_KILLS.updateValue(board, this);
     }
 
     public void addDeath(){
         deaths++;
         PlayerScoreboard board = ScoreboardUtils.getPlayerScoreboard(player.getUniqueId());
         PlayerScoreboard.UpdatingValue.PROFILE_DEATHS.updateValue(board, this);
-        PlayerScoreboard.UpdatingValue.PROFILE_DEATHS.updateValue(board, this);
     }
 
     public void removeDeath(){
         deaths--;
         PlayerScoreboard board = ScoreboardUtils.getPlayerScoreboard(player.getUniqueId());
-        PlayerScoreboard.UpdatingValue.PROFILE_DEATHS.updateValue(board, this);
         PlayerScoreboard.UpdatingValue.PROFILE_DEATHS.updateValue(board, this);
     }
 
@@ -77,6 +63,10 @@ public abstract class MinigamePlayerProfile {
 
     public OfflinePlayer getPlayer(){
         return player;
+    }
+
+    public ArenaContainer getArenaContainer() {
+        return arenaContainer;
     }
 
     public abstract int getShardsEarned();
