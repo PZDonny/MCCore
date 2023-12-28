80ce08a68a1f257a0b52ea5ff6780c550c6c6d04
Jay <76460079+DonnyPZ@users.noreply.github.com>
2023-12-27 20:45:45 -0500

    Changed the algorithm for in isLowestBlock()

--- src/main/java/MCCore/utils/WorldTools.java
+++ src/main/java/MCCore/utils/WorldTools.java
@@ -1,47 +1,39 @@
 package MCCore.utils;
 
 import MCCore.Core;
-import org.bukkit.*;
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 public class WorldTools {
     public static boolean isLowestBlock(Location loc1) {
-        Location loc2 = loc1.clone();
-        loc2.setY(-64);
-        List<Block> blocks = new ArrayList<>();
+        Stack<Block> blocks = new Stack<>();
+        int x = loc1.getBlockX();
+        int z = loc1.getBlockZ();
 
-        int topBlockX = (Math.max(loc1.getBlockX(), loc2.getBlockX()));
-        int bottomBlockX = (Math.min(loc1.getBlockX(), loc2.getBlockX()));
+        for(int y = loc1.getBlockY(); y >= -64; y--){
+            Block block = loc1.getWorld().getBlockAt(x, y, z);
+            blocks.push(block);
+        }
 
-        int topBlockY = (Math.max(loc1.getBlockY(), loc2.getBlockY()));
-        int bottomBlockY = (Math.min(loc1.getBlockY(), loc2.getBlockY()));
-
-        int topBlockZ = (Math.max(loc1.getBlockZ(), loc2.getBlockZ()));
-        int bottomBlockZ = (Math.min(loc1.getBlockZ(), loc2.getBlockZ()));
-
-        for(int x = bottomBlockX; x <= topBlockX; x++)
-        {
-            for(int z = bottomBlockZ; z <= topBlockZ; z++)
-            {
-                for(int y = bottomBlockY; y <= topBlockY; y++)
-                {
-                    Block block = loc1.getWorld().getBlockAt(x, y, z);
-
-                    blocks.add(block);
+        Block lowestfullBlock = null;
+        for (int i = 0; i <= blocks.size(); i++){
+            Block b = blocks.pop();
+            if (!Items.isAir(b.getType()) && !b.isPassable()){
+                if (b.getY() < loc1.getBlockY()){
+                    return false;
                 }
+                lowestfullBlock = b;
             }
         }
-
-        for (Block b : blocks){
-            if (!b.getType().equals(Material.AIR)) return false;
-        }
-        return true;
+        return loc1.getBlock().equals(lowestfullBlock);
     }
 
     public static void destroyWorld(World bukkitWorld) {
@@ -66,7 +58,5 @@ public class WorldTools {
                 Bukkit.unloadWorld(bukkitWorld, true);
             }
         }.runTaskLater(Core.getInstance(), 5);
-
     }
-
 }
