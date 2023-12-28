f8e86e9420a1f6c7e980b3b715bfced92c6e6620
Jay <76460079+DonnyPZ@users.noreply.github.com>
2023-12-27 21:22:21 -0500

    Concise variable naming and updated setCosmeticResetSlot()

--- src/main/java/MCCore/utils/InventoryUtils/ShopGUI.java
+++ src/main/java/MCCore/utils/InventoryUtils/ShopGUI.java
@@ -1,6 +1,7 @@
 package MCCore.utils.InventoryUtils;
 
 
+import MCCore.minigameAPI.MinigameHandler;
 import MCCore.utils.Items;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Material;
@@ -9,20 +10,20 @@ import org.bukkit.inventory.ItemStack;
 
 public class ShopGUI extends ChestGUI{
 
-    final String cosmeticUnlockList;
+    final String cosmeticUnlockListKey;
     final String cosmeticGroupName;
 
     final String cosmeticSelectedKey;
 
-    public ShopGUI(int rows, String name, String cosmeticUnlockList, String cosmeticSelectedKey, String cosmeticGroupName) {
+    public ShopGUI(int rows, String name, String cosmeticUnlockListKey, String cosmeticSelectedKey, String cosmeticGroupName) {
         super(rows, name);
-        this.cosmeticUnlockList = cosmeticUnlockList;
+        this.cosmeticUnlockListKey = cosmeticUnlockListKey;
         this.cosmeticSelectedKey = cosmeticSelectedKey;
         this.cosmeticGroupName = cosmeticGroupName;
     }
 
-    public String getCosmeticUnlockList() {
-        return cosmeticUnlockList;
+    public String getCosmeticUnlockListKey() {
+        return cosmeticUnlockListKey;
     }
 
     public String getCosmeticGroupName() {
@@ -33,11 +34,15 @@ public class ShopGUI extends ChestGUI{
         return cosmeticSelectedKey;
     }
 
-    public void setCosmeticResetSlot(int slot, Object resetValue){
-        ItemStack resetItem = Items.makeItem(Material.STRUCTURE_VOID, 1, ChatColor.RED+"Reset "+cosmeticGroupName);
+    public void setCosmeticResetSlot(int slot, Object resetValue, MinigameHandler handler){
+        setCosmeticResetSlot(slot, resetValue, Material.STRUCTURE_VOID, handler);
+    }
+
+    public void setCosmeticResetSlot(int slot, Object resetValue, Material material, MinigameHandler handler){
+        ItemStack resetItem = Items.makeItem(material, 1, ChatColor.RED+"Reset "+cosmeticGroupName);
         Items.setLore(resetItem, new String[]{ChatColor.YELLOW+"Click to reset your selected "+cosmeticGroupName});
         new GUIItem(this, slot, resetItem, click ->{
-            ShopUtils.resetCosmetic((Player) click.getWhoClicked(), cosmeticSelectedKey, resetValue, cosmeticGroupName);
+            ShopUtils.resetCosmetic((Player) click.getWhoClicked(), cosmeticSelectedKey, resetValue, cosmeticGroupName, handler);
         });
     }
 }
