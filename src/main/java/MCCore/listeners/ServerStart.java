cd2d9d925c25e1c3b8c852b304e583fcac6dd111
Jay <76460079+DonnyPZ@users.noreply.github.com>
2023-12-27 21:08:31 -0500

    Retrieve Mongo Databases and Collections at ServerStart

--- src/main/java/MCCore/listeners/ServerStart.java
+++ src/main/java/MCCore/listeners/ServerStart.java
@@ -1,5 +1,7 @@
 package MCCore.listeners;
 
+import MCCore.Core;
+import MCCore.MongoUtils;
 import MCCore.cosmetics.CosmeticLoader;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,11 +9,18 @@ import org.bukkit.event.server.ServerLoadEvent;
 
 
 public class ServerStart implements Listener {
-
     @EventHandler
     public void onServerStart(ServerLoadEvent event){
-        if (event.getType() == ServerLoadEvent.LoadType.RELOAD) return;
+        if (event.getType() == ServerLoadEvent.LoadType.RELOAD){
+            return;
+        }
         CosmeticLoader.loadAllLoaders();
-
+        String cString = Core.getConnectionString();
+        String mainDB = Core.getMainDatabaseName();
+        String playtestDB = Core.getPlaytestDatabaseName();
+        String minigameDB = Core.getMinigameDatabaseName();
+        if (Core.isMongoAllowed()){
+            MongoUtils.connectToMongo(cString, mainDB, playtestDB, minigameDB);
+        }
     }
 }
