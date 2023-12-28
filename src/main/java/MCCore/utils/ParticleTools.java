842adcc5e9e7a3a3ad375a00121b7d394efce7fc
Jay <76460079+DonnyPZ@users.noreply.github.com>
2023-12-27 21:14:33 -0500

    Made Particles Move smoother depending on distance and speed

--- src/main/java/MCCore/utils/ParticleTools.java
+++ src/main/java/MCCore/utils/ParticleTools.java
@@ -9,16 +9,17 @@ public class ParticleTools {
 
 //Draw Particle Line To/From Locations
     public static void drawParticleLine(Location from, Location to, Particle particle, int particleAmnt, double extra) {
-        for(double i = 0.5; i<from.distance(to); i+= 0.25) {
-            Vector vector  = from.clone().toVector().subtract(to.toVector()).normalize().multiply(-i);
-            Location loc = from.clone().add(vector);
-            loc.getWorld().spawnParticle(particle, loc, particleAmnt, 0, 0, 0, extra);
-        }
+        drawParticleLine(from, to, particle, particleAmnt, 0, 0, 0, extra);
     }
 
 //Draw Particle Line To/From Locations WITH Offsets
     public static void drawParticleLine(Location from, Location to, Particle particle, int particleAmnt, double x, double y, double z, double extra) {
-        for(double i = 0.5; i<from.distance(to); i+= 0.25) {
+        double distance = from.distance(to);
+        double speed = distance/20;
+        if (speed == 0){
+            return;
+        }
+        for(double i = speed; i<distance; i+= speed) {
             Vector vector  = from.clone().toVector().subtract(to.toVector()).normalize().multiply(-i);
             Location loc = from.clone().add(vector);
             loc.getWorld().spawnParticle(particle, loc, particleAmnt, x, y, z, extra);
@@ -28,16 +29,17 @@ public class ParticleTools {
 
 //Draw Redstone Particle Line To/From Locations
     public static void drawRedstoneLine(Location from, Location to, Color color, int particleAmnt, float size) {
-        for(double i = 0.5; i<from.distance(to); i+= 0.25) {
-            Vector vector  = from.clone().toVector().subtract(to.toVector()).normalize().multiply(-i);
-            Location loc = from.clone().add(vector);
-            loc.getWorld().spawnParticle(Particle.REDSTONE, loc, particleAmnt, 0.1, 0.1, 0.1, new Particle.DustOptions(color, size));
-        }
+        drawRedstoneLine(from, to, color,particleAmnt, size, 0.1, 0.1, 0.1);
     }
 
-//Draw Redstone Particle Line To/From Locations WITH Offsets
+//Draw Redstone Particle Line To/From Locatiexecuter.sendMessage(ChatColor.GREEN+"+"+ChatColor.DARK_AQUA+"Weighted Boots!");WITH Offsets
     public static void drawRedstoneLine(Location from, Location to, Color color, int particleAmnt, float size, double x, double y, double z) {
-        for(double i = 0.5; i<from.distance(to); i+= 0.25) {
+        double distance = from.distance(to);
+        double speed = distance/20;
+        if (speed == 0){
+            return;
+        }
+        for(double i = speed; i<distance; i+= speed) {
             Vector vector  = from.clone().toVector().subtract(to.toVector()).normalize().multiply(-i);
             Location loc = from.clone().add(vector);
             loc.getWorld().spawnParticle(Particle.REDSTONE, loc, particleAmnt, x, y, z, new Particle.DustOptions(color, size));
