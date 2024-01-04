2843fef87b5bc87973d0581b7f373dc5c9521009
Jay <76460079+DonnyPZ@users.noreply.github.com>
2024-01-03 19:13:51 -0600

    No Changes

--- src/main/java/MCCore/utils/NoteBlockUtils.java
+++ src/main/java/MCCore/utils/NoteBlockUtils.java
@@ -7,23 +7,31 @@ public class NoteBlockUtils {
 
 //Request Songs
     public static void requestSong(String songName, String requestTag){
-        if (!Core.isNBAPIInstalled()) return;
+        if (!Core.isNBAPIInstalled()){
+            return;
+        }
         Core.getClient().sendMessage(new String[]{Messages.NBAPI_GETSONG.getID(), songName, requestTag});
     }
 
     public static void requestSong(String songName, String folderName, String requestTag){
-        if (!Core.isNBAPIInstalled()) return;
+        if (!Core.isNBAPIInstalled()){
+            return;
+        }
         Core.getClient().sendMessage(new String[]{Messages.NBAPI_GETSONG.getID(), folderName+"/"+songName, requestTag});
     }
 
 //Get All Song Names
     public static void getAllSongNames(String requestTag){
-        if (!Core.isNBAPIInstalled()) return;
+        if (!Core.isNBAPIInstalled()){
+            return;
+        }
         Core.getClient().sendMessage(new String[]{Messages.NBAPI_GETSONGNAMES.getID(), requestTag, ""});
     }
 
     public static void getAllSongNames(String folderName, String requestTag){
-        if (!Core.isNBAPIInstalled()) return;
+        if (!Core.isNBAPIInstalled()){
+            return;
+        }
         Core.getClient().sendMessage(new String[]{Messages.NBAPI_GETSONGNAMES.getID(), requestTag, folderName});
     }
 
